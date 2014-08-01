426f9fff2311cd8e834d9bf8cbc532f7afb36594
Joseph Lenox <lenox.joseph@gmail.com>
2014-08-01 00:21:08 -0500

    Moved HashFunc functionality into a CuckooTable class, which holds pointers to necessary arrays.

--- cuckoo.h
+++ cuckoo.h
@@ -16,7 +16,6 @@ HEMI_DEFINE_STATIC_CONSTANT(long long lg_prime,334214459);
 HEMI_DEFINE_CONSTANT(unsigned long long EMPTY_SLOT, 0xffffffff00000000);
 
 // rebuild flag for our array. If this is set true, there was a failure to add. 
-hemi::Array<bool> rebuild(1, true); 
 
 HEMI_DEFINE_CONSTANT(unsigned int EMPTY_KEY,0xffffffff);
 
@@ -30,6 +29,7 @@ HEMI_DEV_CALLABLE_INLINE unsigned long long make_entry(unsigned int key, unsigne
 
 hemi::Array<unsigned int>hash_a(HEMI_CONSTANT(MAX_FUNCS), true);
 hemi::Array<unsigned int>hash_b(HEMI_CONSTANT(MAX_FUNCS), true);
+hemi::Array<bool> rebuild(1, true); 
 
 class HashFuncs
 {
@@ -43,8 +43,39 @@ class HashFuncs
 
 };
 
+class CuckooTable
+{
+  const unsigned* hash_a;
+  const unsigned* hash_b;
+  bool* g_rebuild;
+  bool* h_rebuild;
+  public: 
+   unsigned long long* table; 
+  CuckooTable(const unsigned* a, const unsigned* b,  unsigned long long* t, bool* grb, bool* hrb) : hash_a(a), hash_b(b), table(t), g_rebuild(grb), h_rebuild(hrb) { } 
+  HEMI_DEV_CALLABLE_INLINE_MEMBER unsigned int hashfunc(unsigned key, unsigned slots, unsigned int n) const {
+    return (hash_a[n] * key + hash_b[n]) % HEMI_CONSTANT(lg_prime) % slots;
+  }
+  HEMI_DEV_CALLABLE_INLINE_MEMBER bool needsRebuild() const
+  {
+    #ifdef HEMI_DEV_CODE
+      return *g_rebuild;
+    #else
+      return *h_rebuild;
+    #endif
+  }
+  HEMI_DEV_CALLABLE_INLINE_MEMBER void setRebuild()
+  {
+    #ifdef HEMI_DEV_CODE
+      *g_rebuild = true;
+    #else
+      *h_rebuild = true;
+    #endif
+  }
+
+};
 void load_hashvals(unsigned int seed = 0)
 {
+  rebuild.writeOnlyHostPtr()[0] = false;
   if (seed != 0) 
     srand(seed);
   else
@@ -55,7 +86,7 @@ void load_hashvals(unsigned int seed = 0)
 	}
 }
 
-HEMI_DEV_CALLABLE_INLINE unsigned long long int retrieve_hash(unsigned long long* table,unsigned int k,unsigned int v, unsigned int SIZE, const HashFuncs hash) {
+HEMI_DEV_CALLABLE_INLINE unsigned long long int retrieve_hash(unsigned long long* table,unsigned int k,unsigned int v, unsigned int SIZE, const CuckooTable hash) {
 	unsigned int location_0 = hash.hashfunc(k,SIZE,0);
 	unsigned int location_1 = hash.hashfunc(k,SIZE,1);
 	unsigned int location_2 = hash.hashfunc(k,SIZE,2);
@@ -75,13 +106,14 @@ HEMI_DEV_CALLABLE_INLINE unsigned long long int retrieve_hash(unsigned long long
 }
 void initCuckooArray(unsigned long long* table, unsigned int SIZE)
 {
+  rebuild.writeOnlyHostPtr()[0] = false;
   for (int i = 0; i < SIZE; i++) 
   {
     table[i] = make_entry(HEMI_CONSTANT(EMPTY_KEY), 0);
   }
 }
 // returns whether or not we were successful.
-HEMI_DEV_CALLABLE_INLINE bool insert_hash(unsigned long long* table, unsigned int k, unsigned int v, unsigned int SIZE, const HashFuncs hash) {
+HEMI_DEV_CALLABLE_INLINE bool insert_hash(unsigned long long* table, unsigned int k, unsigned int v, unsigned int SIZE, CuckooTable hash) {
 	unsigned long long entry = make_entry(k,v); // initial value to place into the table
 	if (retrieve_hash(table, k,v, SIZE, hash) == entry) return true; // already in the table
 	unsigned int key = k;
@@ -122,11 +154,12 @@ HEMI_DEV_CALLABLE_INLINE bool insert_hash(unsigned long long* table, unsigned in
 		else location = location_0;
 
 	}
+  hash.setRebuild();
 	return false; // too many attempts.
 }
 
 // just rebuilds the table itself. If the second int was an index, more problems.
-HEMI_DEV_CALLABLE_INLINE void rebuild_table(unsigned long long* table, unsigned int SIZE, const HashFuncs hash) {
+HEMI_DEV_CALLABLE_INLINE void rebuild_table(unsigned long long* table, unsigned int SIZE, const CuckooTable hash) {
 	for (unsigned i = 0; i < SIZE; i++) {
 		if (table[i] != make_entry(HEMI_CONSTANT(EMPTY_KEY), 0)) // this can be distributed on the GPU, I think.
 			insert_hash(table, get_key(table[i]), get_value(table[i]), SIZE, hash);
--- cuckoo_test.cpp
+++ cuckoo_test.cpp
@@ -14,16 +14,26 @@ HEMI_KERNEL(testCuckooInit)(const unsigned long long* table, unsigned SIZE)
 }
 
 // short test function to 
-HEMI_KERNEL(testCuckooInsert)(unsigned long long* array, unsigned key, unsigned item, HashFuncs hf)
+HEMI_KERNEL(testCuckooInsert)(unsigned key, unsigned item, CuckooTable hf)
 {
   bool test;
   unsigned long long testEntry;
   unsigned long long origEntry = make_entry(key, item);
-  test = insert_hash(array, key, item, HT_SIZE, hf);
-  testEntry = retrieve_hash(array, key, item, HT_SIZE, hf);
+  test = insert_hash(hf.table, key, item, HT_SIZE, hf);
+  testEntry = retrieve_hash(hf.table, key, item, HT_SIZE, hf);
   printf("Insert of (%d, %d): %s\n", key, item, (test == true ? "Success" : "Failure"));
   printf("Hash value retrieved: %lx - (%x, %x) -- %lx\n", testEntry, get_key(testEntry), get_value(testEntry), origEntry);
   assert(testEntry == origEntry);
+}
+HEMI_KERNEL(testCuckooInsertTooMany)(unsigned key, unsigned item, CuckooTable hf)
+{
+  bool test;
+  unsigned long long testEntry;
+  unsigned long long origEntry = make_entry(key, item);
+  test = insert_hash(hf.table, key, item, HT_SIZE, hf);
+  testEntry = retrieve_hash(hf.table, key, item, HT_SIZE, hf);
+  printf("Insert of (%d, %d): %s\n", key, item, (test == true ? "Success" : "Failure"));
+  printf("Hash value retrieved: %lx - (%u, %u) -- %lx\n", testEntry, get_key(testEntry), get_value(testEntry), origEntry);
 }
 
 int main() 
@@ -31,16 +41,29 @@ int main()
   hemi::Array<unsigned long long> hashTable(HT_SIZE, false);
   // create an obj for the hash functions, pointing at
   // the correct location.
+  // Apparently, you have to load data into the host before calling the generic ptr() functions.
+  // This probably has something to do with the lazy update.
   printf("Initializing data...\n");
   load_hashvals();
-  HashFuncs hf = HashFuncs(hash_a.readOnlyPtr(), hash_b.readOnlyPtr()); 
 
 
   unsigned gridDim = 1, blockDim = 1;
   int testInsert = 5;
   int key = 23423;
   initCuckooArray(hashTable.writeOnlyHostPtr(), HT_SIZE);
+  CuckooTable hf = CuckooTable(hash_a.readOnlyPtr(), hash_b.readOnlyPtr(),hashTable.ptr(),rebuild.ptr(),rebuild.ptr(hemi::host)); 
+  printf("Data initialized.\n");
   HEMI_KERNEL_LAUNCH(testCuckooInit, gridDim, blockDim, 0, 0, hashTable.readOnlyPtr(), HT_SIZE);
-  HEMI_KERNEL_LAUNCH(testCuckooInsert, gridDim, blockDim, 0, 0, hashTable.ptr(), key, testInsert, hf);
+  HEMI_KERNEL_LAUNCH(testCuckooInsert, gridDim, blockDim, 0, 0, key, testInsert, hf);
+  for (int i = 0; i < HT_SIZE+1; i++) {
+    HEMI_KERNEL_LAUNCH(testCuckooInsertTooMany, gridDim, blockDim, 0, 0, i * 73, i, hf);
+    if (rebuild.readOnlyPtr(hemi::host)[0] == true)
+    {
+      // we have run into a problem 
+      printf("Detected insert failure for table of size %d! Need to generate new functions and try again.\n");
+      rebuild.writeOnlyPtr(hemi::host)[0] == false;
+      i = HT_SIZE+1; continue;
+    }
+  }
   return 0;
 }
